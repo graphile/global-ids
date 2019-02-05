022a5c9c052d7b79b0bc27ba680512db653b6fb0
Benjie Gillam <benjie@jemjie.com>
2019-02-05 10:32:07 +0000

    Nullable FKs

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,67 @@
 import { Plugin } from "postgraphile";
+import { PgConstraint, PgAttribute, PgClass } from "graphile-build-pg";
 
-const GlobalIdExtensionsPlugin: Plugin = function() {
-  // NOOP
+function isForeignKey(c: PgConstraint): boolean {
+  return c.type === "f";
+}
+
+function containsColumn(c: PgConstraint, attr: PgAttribute): boolean {
+  return c.keyAttributes.includes(attr);
+}
+
+const GlobalIdExtensionsPlugin: Plugin = function(builder) {
+  // Find the relevant input types:
+  //
+  // - FooInput
+  // - FooBaseInput
+  // - FooPatch
+  //
+  // Apply changes:
+  //
+  // - Make foreign key fields optional (not required)
+  // - Add optional node identifier fields
+  //
+  // Finally wrap the resolver to overwrite the relevant args.
+
+  builder.hook(
+    "GraphQLInputObjectType:fields:field",
+    function MakeForeignKeyInputFieldsNullable(field, build, context) {
+      const {
+        graphql: { getNullableType },
+      } = build;
+      const {
+        scope: {
+          isPgRowType,
+          isInputType,
+          pgIntrospection,
+          pgFieldIntrospection,
+        },
+      } = context;
+
+      if (
+        !isPgRowType ||
+        !isInputType ||
+        pgIntrospection.kind !== "class" ||
+        pgFieldIntrospection.kind !== "attribute"
+      ) {
+        return field;
+      }
+      const table: PgClass = pgIntrospection;
+      const attr: PgAttribute = pgFieldIntrospection;
+
+      // If this field belongs to a foreign key, mark it nullable.
+      if (
+        table.constraints.some(c => isForeignKey(c) && containsColumn(c, attr))
+      ) {
+        return {
+          ...field,
+          type: getNullableType(field.type),
+        };
+      }
+
+      return field;
+    }
+  );
 };
 
 export default GlobalIdExtensionsPlugin;
