ecfec668061803ea1244e638bc186301c8d74c92
Benjie Gillam <benjie@jemjie.com>
2019-02-05 10:31:10 +0000

    Use ts-jest preset

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  transform: {
-    "^.+\\.ts$": "ts-jest",
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.ts$",
-  moduleFileExtensions: ["ts", "js", "json"],
+  preset: "ts-jest",
 };
